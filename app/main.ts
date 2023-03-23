d971c8df10b6571dc66cdeb41006759c1fb39912
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 10:11:38 +0300

    Compiled the ts file to a comparable js file

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,1 @@
-"use strict"
-let username = 'Kijana Mangaa atanunulia mtoto changaa'
-
-console.log(username)
-
-
-let a: number = 12
-let b: string = '6'
-let c: number = 2
-
-console.log(a / b)
-console.log(c * b)
+let myName = "Dave"
