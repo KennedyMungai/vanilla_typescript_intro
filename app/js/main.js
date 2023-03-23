9cfe4239fa0ac29ff0682f66bfc6cf896269e151
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 10:23:30 +0300

    Console logged the first element of the stringArr

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
 "use strict";
 let stringArr = ['Chicken', 'Wings', 'Are', 'Awesome'];
 let guitars = ['Strat', 'Les Paul', 5150];
+console.log(stringArr[0]);
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
 let stringArr: string[] = ['Chicken', 'Wings', 'Are', 'Awesome']
 
 let guitars: (string | number)[] = ['Strat', 'Les Paul', 5150]
+
+console.log(stringArr[0])
