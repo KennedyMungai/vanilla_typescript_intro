cecc45286bfad2ec7b1861fa5f9d7867a0ea80bd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 10:22:41 +0300

    Defined two arraus for the typescript file

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,3 @@
 "use strict";
 let stringArr = ['Chicken', 'Wings', 'Are', 'Awesome'];
+let guitars = ['Strat', 'Les Paul', 5150];
--- app/main.ts
+++ app/main.ts
@@ -1,1 +1,3 @@
 let stringArr: string[] = ['Chicken', 'Wings', 'Are', 'Awesome']
+
+let guitars: (string | number)[] = ['Strat', 'Les Paul', 5150]
