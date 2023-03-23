d87b02b519d664c98d2103e99c9b198fc8069c76
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 10:04:54 +0300

    Added some more code for testing purposes

--- app/main.ts
+++ app/main.ts
@@ -2,4 +2,11 @@
 let username = 'Kijana Mangaa atanunulia mtoto changaa'
 
 console.log(username)
-console.log(username)
+
+
+let a: number = 12
+let b: number = 6
+let c: number = 2
+
+console.log(a / b)
+console.log(c * b)
